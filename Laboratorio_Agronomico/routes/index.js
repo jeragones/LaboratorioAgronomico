bc1777fa8571dbd2ed3aebd9e14c132a4a4067f3
Daniel Berrocal <jdanielb1992@gmail.com>
2014-03-31 19:34:29 -0600

    animated image

--- Laboratorio_Agronomico/routes/index.js
+++ Laboratorio_Agronomico/routes/index.js
@@ -4,10 +4,5 @@
  */
 
 exports.index = function(req, res){
-<<<<<<< HEAD
-  res.render('news', { title: 'LAAG' });
-};
-=======
 	res.render('news'/*, { title: 'Express'/*, dynamic : dynamicContent }*/);
 };
->>>>>>> c21956b3de42b6d9dafd762f008ed68677d71e1b
