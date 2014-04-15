c22adeeb4facbfc226540fc3de85df12c0aefac6
Daniel Berrocal <jdanielb1992@gmail.com>
2014-04-15 12:12:30 -0600

    responsables fail

---
+++ Laboratorio_Agronomico/public/javascripts/responsables.js
@@ -0,0 +1,23 @@
+
+$( document ).ready(function() {
+
+	$("#opResponsible").click(function(){
+	
+		alert("1");
+
+
+
+		/*cliente.connect();
+
+		var query = cliente.query(
+			'SELECT * from usuario',function(err,result,fields){
+				if (err) trow err;
+				console.log('hola');
+		});
+
+		cliente.end();
+		*/
+	});
+
+});
+
