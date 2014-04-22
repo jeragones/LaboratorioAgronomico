e6603d6f5c453ea1fe326df9f6da4cf6855b12d1
Daniel Berrocal <jdanielb1992@gmail.com>
2014-04-21 21:09:51 -0600

    First Resposibles

--- Laboratorio_Agronomico/public/javascripts/navigation.js
+++ Laboratorio_Agronomico/public/javascripts/navigation.js
@@ -28,6 +28,7 @@ $( document ).ready(function() {
 
     $("#opResponsible").click(function() {
         $("#containerBody").load("responsible.html");
+        resp();
     });
 
     $("#opContact").click(function() {
--- Laboratorio_Agronomico/public/javascripts/responsables.js
+++ Laboratorio_Agronomico/public/javascripts/responsables.js
@@ -9,40 +9,54 @@ var socket = io.connect('http://localhost:3000');
 
 });*/
 
-$("#resp").click(function(){
-	
+function resp(){
+	$("#responsibles").empty();
 	socket.emit('databaseAction', { query : "select nombre, apellido1, apellido2, correo, puesto, imagen, descripcion  from persona inner join usuario on persona.id_persona = usuario.id_persona" });
-		socket.on('databaseAction', function(data) { 
-			if(data.error) {
-				alert("ocurrio un error en la consulta");
-			} else {
-				//$("#responsibles").empty();
-				var nombre, apellido1,apellido2,correo="",puesto,imagen="",descripcion="";
-				for (var x in data.data) {
-					nombre = (data.data[x].nombre).toString();
+	socket.on('databaseAction', function(data) { 
+		if(data.error) {
+			alert("ocurrio un error en la consulta");
+		} else {
+			
+			var nombre, apellido1,apellido2,correo="",puesto,imagen="",descripcion="";
+			for (var x in data.data) {
+				nombre = (data.data[x].nombre).toString();
 
-					apellido1 = (data.data[x].apellido1).toString();
-					apellido2 = (data.data[x].apellido2).toString();
-					if(data.data[x].correo != null) {
-						correo = (data.data[x].correo).toString();
-					}
-					puesto = (data.data[x].puesto).toString();
-					if(data.data[x].imagen != null){
-						imagen = (data.data[x].imagen).toString();
-					}
-					else{
-						imagen = "/images/Administrator.png";
-					}
-					
-					if(data.data[x].descripcion != null){
-						descripcion = (data.data[x].descripcion).toString();
-					}
-
-					$("#responsibles").append( "<article'>" +"<img src="+"'"+imagen+"'"+">" + nombre + ", " + apellido1 + ", " + apellido2 + ", " + correo + ", " + puesto + "," + descripcion + "</article>" );
+				apellido1 = (data.data[x].apellido1).toString();
+				apellido2 = (data.data[x].apellido2).toString();
+				if(data.data[x].correo != null) {
+					correo = (data.data[x].correo).toString();
 				}
+				puesto = (data.data[x].puesto).toString();
+				if(data.data[x].imagen != null){
+					imagen = (data.data[x].imagen).toString();
+				}
+				else{
+					imagen = "/images/pasto3.png";
+				}
+				
+				if(data.data[x].descripcion != null){
+					descripcion = (data.data[x].descripcion).toString();
+				}
+
+				$("#responsibles").append( 
+					"<article class='resp'>" +
+					"<img src="+"'"+imagen+"'"+">" + 
+					"<div>"+
+						"<h2>"+nombre + " " + apellido1 +" "+ apellido2 + "</h2>" +
+						"<label>"+ puesto + "</label>"+ 
+						"<p>"+ descripcion + "</p>"+
+						"<a href=mailto:"+correo+">"+ correo + "</a>"+ 
+					"</div>"+ 
+					"</article>" );
 			}
-		});
-	
+		}
+	});
+
+}
+
+
+$("#resp").click(function(){
+resp();
 
 	/*for(var r in q){
 		var newarticle = document.createElement('article');
@@ -57,10 +71,6 @@ $("#resp").click(function(){
 	newarticle.innerHTML= "<article>daniel</article>";
 	
 	section.appendChild(newarticle);
-	*/	
-
-	
-	
-	
+	*/		
 	});
 
